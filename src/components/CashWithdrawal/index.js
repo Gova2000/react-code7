066a55e4a88b63fab5d0707eeac8863c4543b1cb
Gova2000 <Govardhanmanda2000@gmail.com>
2023-08-18 11:32:27 +0000

    withdraw

--- src/components/CashWithdrawal/index.js
+++ src/components/CashWithdrawal/index.js
@@ -1,27 +1,51 @@
 // Write your code here
-
 import './index.css'
 
-const Cash = props => {
-  const {Details, minus} = props
-  const {value} = Details
+import {Component} from 'react'
 
-  const Mins = () => {
-    minus(value)
+import Cash from '../DenominationItem'
+
+class CashWithdrawal extends Component {
+  state = {Amount: 2000}
+
+  ChangeAmount = value => {
+    const {Amount} = this.state
+
+    this.setState({Amount: Amount - value})
   }
 
-  return (
-    <div className="Let">
-      <li>
-        <button type="button" onChange={Mins}>
-          {value}
-        </button>
-        <button type="button" onChange={Mins}>
-          {value}
-        </button>
-      </li>
-    </div>
-  )
+  render() {
+    const {denominationsList} = this.props
+    const {Amount} = this.state
+
+    return (
+      <div className="card">
+        <div className="card1">
+          <div className="name1">
+            <p className="design">S</p>
+            <p className="name">Sarah Williams</p>
+          </div>
+          <div className="mony">
+            <p className="blnc">Your Balance</p>
+            <div className="not">
+              <p className="amt">
+                {Amount} <span className="amtxt">in Rupees</span>
+              </p>
+            </div>
+          </div>
+          <div className="withcard">
+            <p className="amt">Withdraw</p>
+            <p className="blnc">CHOOSE SUM (IN RUPEES)</p>
+            <ul className="but">
+              {denominationsList.map(each => (
+                <Cash Details={each} minus={this.ChangeAmount} key={each.id} />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Cash
+export default CashWithdrawal
--- src/components/DenominationItem/index.js
+++ src/components/DenominationItem/index.js
@@ -1,52 +1,22 @@
 // Write your code here
+
 import './index.css'
 
-import {Component} from 'react'
+const Cash = props => {
+  const {Details, minus} = props
+  const {value} = Details
 
-import Cash from '../CashWithdrawal'
-
-class CashWithdrawal extends Component {
-  state = {Amount: 2000}
-
-  ChangeAmount = value => {
-    const {Amount} = this.state
-
-    this.setstate({Amount: Amount - value})
+  const Mins = () => {
+    minus(value)
   }
 
-  render() {
-    const {denominationsList} = this.props
-    const {Amount} = this.state
-
-    return (
-      <div className="card">
-        <div className="card1">
-          <div>
-            <p className="design">S</p>
-            <p className="name">Sarah Williams</p>
-          </div>
-          <div>
-            <p className="blnc">Your Balance</p>
-            <p className="amt">
-              {Amount} <span className="amtxt">in Rupees</span>
-            </p>
-          </div>
-          <div>
-            <h1 className="amtxt">Withdraw</h1>
-            <p className="blnc">CHOOSE SUM (IN RUPEES)</p>
-            <ul>
-              {denominationsList.map((each) => ({
-                <Cash Details={each} minus={this.ChangeAmount} key={each.id} />
-              }))}
-            </ul>
-          </div>
-        </div>
-      </div>
-    )
-  }
+  return (
+    <li>
+      <button type="button" onClick={Mins}>
+        {value}
+      </button>
+    </li>
+  )
 }
 
-export default CashWithdrawal
-
-
-
+export default Cash
